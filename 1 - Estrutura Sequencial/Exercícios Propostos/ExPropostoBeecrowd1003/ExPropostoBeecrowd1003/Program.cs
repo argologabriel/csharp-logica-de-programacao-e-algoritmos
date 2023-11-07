626509650b68197c69269a3f330ec083263b986e
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-07 15:12:31 -0300

    Exercício Proposto Beecrowd 1003

---
+++ 1 - Estrutura Sequencial/Exercícios Propostos/ExPropostoBeecrowd1003/ExPropostoBeecrowd1003/Program.cs
@@ -0,0 +1,32 @@
+﻿/// Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável. ///
+
+using System;
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main(string[] args) {
+
+            // Declaração de Variáveis
+
+            int A, B;
+            int SOMA;
+
+            // Entrada de Dados
+
+            Console.WriteLine("Informe o valor da variável A: ");
+            A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor da variável B: ");
+            B = int.Parse(Console.ReadLine());
+
+            // Processamento de Dados
+
+            SOMA = A + B;
+
+            // Saída de Dados
+
+            Console.WriteLine("SOMA = " + SOMA);
+
+            }
+        }
+    }
