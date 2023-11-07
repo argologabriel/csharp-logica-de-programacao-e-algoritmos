c3bf0c1d03ac6dfbb357926064adec59e91e3b9e
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-07 16:00:36 -0300

    Exercício Proposto Beecrowd 1006

---
+++ 1 - Estrutura Sequencial/Exercícios Propostos/ExPropostoBeecrowd1006/ExPropostoBeecrowd1006/Program.cs
@@ -0,0 +1,36 @@
+﻿/// Leia 3 valores, no caso, variáveis A, B e C, que são as três notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 2, a nota B tem peso 3 e a nota C tem peso 5. Considere que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal. ///
+
+using System;
+using System.Globalization;
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main(string[] args) {
+
+            // Declaração de Variáveis
+
+            double A, B, C;
+            double MEDIA;
+
+            // Entrada de Dados
+
+            Console.WriteLine("Informe a primeira nota (A): ");
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Informe a segunda nota (B): ");
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Informe a terceira nota (C): ");
+            C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Processamento de Dados
+
+            MEDIA = ((A * 2) + (B * 3) + (C * 5)) / 10;
+
+            // Saída de Dados
+
+            Console.WriteLine("MEDIA = " + MEDIA.ToString("F1", CultureInfo.InvariantCulture));
+
+            }
+        }
+    }
