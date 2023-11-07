dc78df7670875009057af8a2966ac61868165d5c
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-07 15:22:19 -0300

    Exercício Proposto Beecrowd 1004

---
+++ 1 - Estrutura Sequencial/Exercícios Propostos/ExPropostoBeecrowd1004/ExPropostoBeecrowd1004/Program.cs
@@ -0,0 +1,32 @@
+﻿/// Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente. ///
+
+using System;
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main(string[] args) {
+
+            // Declaração de Variáveis
+
+            int ValorInteiro1, ValorInteiro2;
+            int PROD;
+
+            // Entrada de Dados;
+
+            Console.WriteLine("Informe o primeiro valor: ");
+            ValorInteiro1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo valor: ");
+            ValorInteiro2 = int.Parse(Console.ReadLine());
+
+            // Processamento de Dados
+
+            PROD = ValorInteiro1 * ValorInteiro2;
+
+            // Saída de Dados
+
+            Console.WriteLine("PROD = " + PROD);
+
+            }
+        }
+    }
