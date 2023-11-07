1e8358ac6f6f8d06aab24067a68bd641ae517115
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-07 15:48:56 -0300

    Exercício Proposto Beecrowd 1005

---
+++ 1 - Estrutura Sequencial/Exercícios Propostos/ExPropostoBeecrowd1005/ExPropostoBeecrowd1005/Program.cs
@@ -0,0 +1,33 @@
+﻿/// Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem a 2 notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11). Assuma que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal. ///
+
+using System;
+using System.Globalization;
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main(string[] args) {
+
+            // Declaração de Variáveis
+
+            double A, B;
+            double MEDIA;
+
+            // Entrada de Dados
+
+            Console.WriteLine("Informe a primeira nota: ");
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Informe a segunda nota: ");
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Processamento de Dados
+
+            MEDIA = ((A * 3.5) + (B * 7.5)) / 11;
+
+            // Saída de Dados
+
+            Console.WriteLine("MEDIA = " + MEDIA.ToString("F5", CultureInfo.InvariantCulture));
+
+            }
+        }
+    }
