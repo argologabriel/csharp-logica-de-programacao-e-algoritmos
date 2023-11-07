b044d26d73cdb4e266374f7d1c9113fd736d1227
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-07 11:35:10 -0300

    Exercício Proposto Beecrowd 1002

---
+++ 1 - Estrutura Sequencial/Exercícios Propostos/ExPropostoBeecrowd1002/ExPropostoBeecrowd1002/Program.cs
@@ -0,0 +1,32 @@
+﻿/// A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159: Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π. ///
+
+using System;
+using System.Globalization;
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main(string[] args) {
+
+            // Declaração de Variáveis
+
+            double pi, raio;
+            double area;
+
+            // Entrada de Dados
+
+            pi = 3.14159;
+
+            Console.WriteLine("Informe o valor do raio: ");
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Processamento de Dados
+
+            area = pi * Math.Pow(raio, 2);
+
+            // Saída de Dados
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
