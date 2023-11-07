c1718a906c541641b6e863774f9cf08325df9247
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-07 15:34:05 -0300

    Exercício Proposto Beecrowd 1007

---
+++ 1 - Estrutura Sequencial/Exercícios Propostos/ExPropostoBeecrowd1007/ExPropostoBeecrowd1007/Program.cs
@@ -0,0 +1,38 @@
+﻿/// Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D) ///
+
+using System;
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main(string[] args) {
+
+            // Declaração de Variáveis
+
+            int A, B, C, D;
+            int DIFERENCA;
+
+            // Entrada de Dados
+
+            Console.WriteLine("Informe o valor de A: ");
+            A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor de B: ");
+            B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor de C: ");
+            C = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor de D: ");
+            D = int.Parse(Console.ReadLine());
+
+            // Processamento de Dados
+
+            DIFERENCA = (A * B) - (C * D);
+
+            // Saída de Dados
+
+            Console.WriteLine("DIFERENCA = " + DIFERENCA);
+
+            }
+        }
+    }
