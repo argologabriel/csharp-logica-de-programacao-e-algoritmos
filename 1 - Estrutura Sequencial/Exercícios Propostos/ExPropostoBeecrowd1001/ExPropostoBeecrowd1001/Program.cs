029572c582f4cc527c4d74bbc92471749182d4ab
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-07 09:55:17 -0300

    Exercício Proposto 1001 Beecrowd

---
+++ 1 - Estrutura Sequencial/Exercícios Propostos/ExPropostoBeecrowd1001/ExPropostoBeecrowd1001/Program.cs
@@ -0,0 +1,31 @@
+﻿/// Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X. ///
+
+using System;
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main(string[] args) {
+
+            // Declaração de Variáveis
+
+            int A, B;
+            int X;
+
+            // Entrada de Dados
+
+            Console.WriteLine("Digite o valor de A: ");
+            A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de B: ");
+            B = int.Parse(Console.ReadLine());
+
+            // Processamento de Dados
+
+            X = A + B;
+
+            // Saída de Dados
+
+            Console.WriteLine("X = " + X);
+        }
+    }
+}
