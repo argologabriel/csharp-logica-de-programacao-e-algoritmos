4883bf5e39f68e430f8d6478f89575be361d6212
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-07 23:03:33 -0300

    Exercício Proposto Beecrowd 1014

---
+++ 1 - Estrutura Sequencial/Exercícios Propostos/ExPropostoBeecrowd1014/ExPropostoBeecrowd1014/Program.cs
@@ -0,0 +1,35 @@
+﻿/// Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km) e o total de combustível gasto (em litros). ///
+
+using System;
+using System.Globalization;
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main (string[] args) {
+
+            // Declaração de Variáveis
+
+            int X; // Distância percorrida
+            double Y; //Combustível gasto
+
+            double consumoMedio;
+
+            // Entrada de Dados
+
+            Console.WriteLine("Informe a distância percorrida (em Km) pelo automóvel: ");
+            X = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o total de combustível gasto (em litros) pelo automóvel: ");
+            Y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Processamento de Dados
+
+            consumoMedio = X / Y;
+
+            // Saída de Dados
+
+            Console.WriteLine(consumoMedio.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+
+            }
+        }
+    }
