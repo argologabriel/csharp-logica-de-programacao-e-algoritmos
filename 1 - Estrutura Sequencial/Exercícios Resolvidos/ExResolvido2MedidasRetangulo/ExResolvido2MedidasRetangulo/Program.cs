9765bb9881ee55cfaba1ee22f89721b01c0e7135
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-08 09:21:49 -0300

    Exercício Resolvido 2 (Medidas Retângulo)

---
+++ 1 - Estrutura Sequencial/Exercícios Resolvidos/ExResolvido2MedidasRetangulo/ExResolvido2MedidasRetangulo/Program.cs
@@ -0,0 +1,41 @@
+﻿/// Fazer um programa para ler as medidas da base e altura de um retângulo. Em seguida, mostrar o valor da área, perímetro e diagonal deste retângulo, com quatro casas decimais ///
+
+using System;
+using System.Globalization;
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main(string[] args) {
+
+            // Declaração de Variáveis
+
+            double BASE, ALTURA;
+            double AREA, PERIMETRO, DIAGONAL;
+
+            // Entrada de Dados 
+
+            Console.WriteLine("Informe o valor da base do retângulo: ");
+            BASE = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Informe o valor da altura do retângulo: ");
+            ALTURA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Processamento de Dados 
+
+            AREA = BASE * ALTURA;
+
+            PERIMETRO = (BASE * 2.0) + (ALTURA * 2.0);
+
+            DIAGONAL = Math.Sqrt(Math.Pow(BASE, 2.0) + Math.Pow(ALTURA, 2.0));
+
+            // Saída de Dados
+
+            Console.WriteLine("AREA = " + AREA.ToString("F4", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("PERIMETRO = " + PERIMETRO.ToString("F4", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("DIAGONAL = " + DIAGONAL.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
