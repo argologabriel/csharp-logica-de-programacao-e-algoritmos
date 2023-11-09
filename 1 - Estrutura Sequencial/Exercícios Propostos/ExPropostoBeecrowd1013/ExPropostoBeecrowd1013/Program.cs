d7f78ef0e7b08007cc812f2badf0512608ca964d
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-08 23:51:47 -0300

    Exercício Proposto Beecrowd 1013

---
+++ 1 - Estrutura Sequencial/Exercícios Propostos/ExPropostoBeecrowd1013/ExPropostoBeecrowd1013/Program.cs
@@ -0,0 +1,34 @@
+﻿// Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
+
+using System;
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main(string[] args) {
+
+            // Declaração de Variáveis
+
+            int a, b, c;
+            int maiorAB, maiorXC;
+
+            // Entrada de Dados
+
+            Console.WriteLine("Digite 3 valores: ");
+            string[] valores = Console.ReadLine().Split(' ');
+
+            a = int.Parse(valores[0]);
+            b = int.Parse(valores[1]);
+            c = int.Parse(valores[2]);
+
+            // Processamento de Dados
+
+            maiorAB = (a + b + Math.Abs(a - b)) / 2;
+            maiorXC = (maiorAB + c + Math.Abs(maiorAB - c)) / 2;
+
+            // Saída de Dados
+
+            Console.WriteLine(maiorXC + " eh o maior");
+
+            }
+        }
+    }
