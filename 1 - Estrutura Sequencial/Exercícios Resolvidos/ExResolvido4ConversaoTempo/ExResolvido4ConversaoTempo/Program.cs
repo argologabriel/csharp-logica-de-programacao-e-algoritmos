2d99bc0af39d52085bff4a3c55d3ec718c0e9736
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-09 08:45:54 -0300

    Exercício Resolvido 4 (Conversão de Tempo)

---
+++ 1 - Estrutura Sequencial/Exercícios Resolvidos/ExResolvido4ConversaoTempo/ExResolvido4ConversaoTempo/Program.cs
@@ -0,0 +1,31 @@
+﻿/// Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, e informe-o expresso no formato horas:minutos:segundos.
+
+using System;
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main(string[] args) {
+
+            // Declaração de Variáveis
+
+            int horas, minutos, segundos, resto;
+
+            // Entrada de Dados
+
+            Console.WriteLine("Informe o tempo de duração do evento (em segundos): ");
+            segundos = int.Parse(Console.ReadLine());
+
+            // Processamento de Dados
+
+            horas = segundos / 3600;
+            resto = segundos % 3600;
+            minutos = resto / 60;
+            segundos = resto % 60;
+
+            // Saída de Dados
+
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+
+        }
+    }
+}
