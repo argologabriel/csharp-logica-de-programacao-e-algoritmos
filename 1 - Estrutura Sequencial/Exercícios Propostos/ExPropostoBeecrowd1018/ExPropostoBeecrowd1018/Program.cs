e92602338d7a98d594daaa18f36fa54c91c05af9
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-09 11:28:54 -0300

    Exercício Proposto Beecrowd 1018

---
+++ 1 - Estrutura Sequencial/Exercícios Propostos/ExPropostoBeecrowd1018/ExPropostoBeecrowd1018/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+Leia um valor inteiro. A seguir, calcule o menor número de notas possíveis (cédulas) no qual o valor pode ser decomposto. As notas consideradas são de 100, 50, 20, 10, 5, 2 e 1. A seguir mostre o valor lido e a relação de notas necessárias.
+ */
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main(string[] args) {
+
+            // Declaração de Variáveis
+
+            int N;
+            int quociente, resto, nota;
+
+            // Entrada de Dados
+
+            Console.WriteLine("Informe o valor de N: ");
+            N = int.Parse(Console.ReadLine());
+
+            // Processamento e Saída de Dados
+
+            resto = N;
+
+            nota = 100;
+            quociente = resto / nota;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 50;
+            quociente = resto / nota;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 20;
+            quociente = resto / nota;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 10;
+            quociente = resto / nota;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 5;
+            quociente = resto / nota;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 2;
+            quociente = resto / nota;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 1;
+            quociente = resto / nota;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+
+        }
+    }
+}
+
