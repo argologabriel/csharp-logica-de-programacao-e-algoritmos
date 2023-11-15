61e06360a61942ba47395715ce2415b04727393a
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-15 14:22:36 -0300

    Exercício Proposto Beecrowd 1046

---
+++ 2 - Estrutura Condicional/Exercícios Propostos/ExPropostoBeecrowd1046/ExPropostoBeecrowd1046/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.
+*/
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main(string[] args) {
+
+            // Declaração de Variáveis
+
+            int inicio, fim;
+            int duracao;
+
+            // Entrada de Dados
+
+            Console.WriteLine("Informe o horário de início e término do jogo: ");
+            string[] valores = Console.ReadLine().Split(' ');
+
+            inicio = int.Parse(valores[0]);
+            fim = int.Parse(valores[1]);
+
+            // Processamento de Dados
+
+            if (fim > inicio) {
+                duracao = fim - inicio;
+            }
+            else {
+                duracao = (fim + 24) - inicio;
+            }
+
+            // Saída de Dados
+
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+        }
+    }
+}
