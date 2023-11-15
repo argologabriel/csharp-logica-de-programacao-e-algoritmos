7546ba696c45e6be1196ccfdd50491dd617dd237
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-15 10:14:32 -0300

    Exercício Proposto Beecrowd 1035

---
+++ 2 - Estrutura Condicional/Exercícios Propostos/ExPropostoBeecrowd1035/ExPropostoBeecrowd1035/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Leia 4 valores inteiros A, B, C e D. A seguir, se B for maior do que C e se D for maior do que A, e a soma de C com D for maior que a soma de A e B e se C e D, ambos, forem positivos e se a variável A for par escrever a mensagem "Valores aceitos", senão escrever "Valores nao aceitos".
+*/
+
+using System;
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main(string[] args) {
+
+            // Declaração de Variáveis
+
+            int A, B, C, D;
+
+            // Entrada de Dados
+
+            Console.WriteLine("Digite 4 valores inteiros: ");
+            string[] valores = Console.ReadLine().Split(' ');
+
+            A = int.Parse(valores[0]);
+            B = int.Parse(valores[1]);
+            C = int.Parse(valores[2]);
+            D = int.Parse(valores[3]);
+
+            // Processamento e Saída de Dados
+
+            if (B > C && D > A && (C + D) > (A + B) && C > 0 && D > 0 && A % 2 == 0) {
+                Console.WriteLine("Valores aceitos");
+            }
+
+            else {
+                Console.WriteLine("Valores nao aceitos");
+            }
+        }
+    }
+}
