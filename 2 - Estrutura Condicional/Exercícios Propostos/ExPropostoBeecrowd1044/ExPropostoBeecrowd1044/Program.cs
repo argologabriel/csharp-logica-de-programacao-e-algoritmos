13d58a3e6d0954c48c3c6f22651d4370d41807de
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-15 13:09:42 -0300

    Exercício Proposto Beecrowd 1044

---
+++ 2 - Estrutura Condicional/Exercícios Propostos/ExPropostoBeecrowd1044/ExPropostoBeecrowd1044/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si.
+*/
+
+using System;
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main(string[] args) {
+
+            // Declaração de Variáveis
+
+            int A, B;
+
+            // Entrada de Dados
+
+            Console.WriteLine("Digite dois valores inteiros: ");
+            string[] valores = Console.ReadLine().Split(' ');
+
+            A = int.Parse(valores[0]);
+            B = int.Parse(valores[1]);
+
+            // Processamento e Saída de Dados
+
+            if (A % B == 0 || B % A == 0) {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
