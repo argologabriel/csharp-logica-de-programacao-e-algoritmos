f217bd5dba1e1b73215c4071936fbc1ed2e1b144
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-22 23:36:03 -0300

    Exercício Proposto Beecrowd 1049

---
+++ 2 - Estrutura Condicional/Exercícios Propostos/ExPropostoBeecrowd1049/ExPropostoBeecrowd1049/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+Neste problema, você deverá ler 3 palavras que definem o tipo de animal possível segundo o esquema abaixo, da esquerda para a direita.  Em seguida conclua qual dos animais seguintes foi escolhido, através das três palavras fornecidas.
+*/
+
+using System;
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main(string[] args) {
+
+            // Declaração de Variáveis
+
+            string palavra1, palavra2, palavra3;
+
+            // Entrada de Dados
+
+            Console.WriteLine("Defina o tipo do animal: ");
+            palavra1 = Console.ReadLine();
+            palavra2 = Console.ReadLine();
+            palavra3 = Console.ReadLine();
+
+            // Processamento e Saída de Dados
+
+            if (palavra1 == "vertebrado") {
+                if (palavra2 == "ave") {
+                    if (palavra3 == "carnivoro") {
+                        Console.WriteLine("aguia");
+                    }
+                    else {
+                        Console.WriteLine("pomba");
+                    }
+                }
+                else {
+                    if (palavra3 == "onivoro") {
+                        Console.WriteLine("homem");
+                    }
+                    else {
+                        Console.WriteLine("vaca");
+                    }
+                }
+            }
+            else {
+                if (palavra2 == "inseto") {
+                    if (palavra3 == "hematofago") {
+                        Console.WriteLine("pulga");
+                    }
+                    else {
+                        Console.WriteLine("lagarta");
+                    }
+                }
+                else {
+                    if (palavra3 == "hematofago") {
+                        Console.WriteLine("sanguessuga");
+                    }
+                    else {
+                        Console.WriteLine("minhoca");
+                    }
+                }
+            }
+        }
+    }
+}
