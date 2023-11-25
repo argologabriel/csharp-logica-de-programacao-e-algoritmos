bc76d5b5b248ecaba5b0f25c84d46fd83c89dd91
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-24 23:10:00 -0300

    Exercício Proposto Beecrowd 1043

---
+++ 2 - Estrutura Condicional/Exercícios Propostos/ExPropostoBeecrowd1043/ExPropostoBeecrowd1043/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Leia 3 valores reais (A, B e C) e verifique se eles formam ou não um triângulo. Em caso positivo, calcule o perímetro do triângulo e apresente a mensagem:
+
+    Perimetro = XX.X
+
+Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura, mostrando a mensagem:
+
+    Area = XX.X
+*/
+
+using System;
+using System.Globalization;
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main(string[] args) {
+
+            // Declaração de Variáveis
+
+            double a, b, c, perimetro, area;
+
+            // Entrada de Dados
+
+            Console.WriteLine("Digite três valores reais: ");
+            String[] vet = Console.ReadLine().Split(' ');
+
+            a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            // Processamento e Saída de Dados
+
+            if (a < b + c && b < a + c && c < a + b) {
+                perimetro = a + b + c;
+                Console.WriteLine("Perimetro = " + perimetro.ToString("F1", CultureInfo.InvariantCulture));
+            }
+            else {
+                area = ((a + b) * c) / 2;
+                Console.WriteLine("Area = " + area.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
