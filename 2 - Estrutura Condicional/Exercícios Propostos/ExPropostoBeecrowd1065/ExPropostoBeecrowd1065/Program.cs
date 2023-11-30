9d63c42a2c5cc76dfa58c9b319a9cbab9580264d
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-29 22:59:55 -0300

    Exercício Proposto Beecrowd 1065

---
+++ 2 - Estrutura Condicional/Exercícios Propostos/ExPropostoBeecrowd1065/ExPropostoBeecrowd1065/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Faça um programa que leia 5 valores inteiros. Conte quantos destes valores digitados são pares e mostre esta informação.
+*/
+
+using System;
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main(string[] args) {
+
+            // Declaração de Variáveis
+
+            int a, b, c, d, e;
+            int contador;
+
+            // Entrada de Dados
+
+            Console.WriteLine("Digite 5 valores inteiros quaisquer: ");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+            e = int.Parse(Console.ReadLine());
+
+            // Processamento de Dados
+
+            contador = 0;
+
+            if (a % 2 == 0) {
+                contador = contador + 1;
+            }
+            if (b % 2 == 0) {
+                contador = contador + 1;
+            }
+            if (c % 2 == 0) {
+                contador = contador + 1;
+            }
+            if (d % 2 == 0) {
+                contador = contador + 1;
+            }
+            if (e % 2 == 0) {
+                contador = contador + 1;
+            }
+
+            // Saída de Dados
+
+            Console.WriteLine(contador + " valores pares");
+        }
+    }
+}
