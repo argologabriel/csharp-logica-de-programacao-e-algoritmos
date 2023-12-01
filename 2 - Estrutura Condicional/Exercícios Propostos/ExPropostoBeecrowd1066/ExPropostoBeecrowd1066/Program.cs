c963135ff0525fc7b0bfb10eaace37eb5612ca52
Gabriel Argôlo <gabriel.argolo@outlook.com>
2023-11-30 23:04:18 -0300

    Exercício Proposto Beecrowd 1066

---
+++ 2 - Estrutura Condicional/Exercícios Propostos/ExPropostoBeecrowd1066/ExPropostoBeecrowd1066/Program.cs
@@ -0,0 +1,96 @@
+﻿/*
+Leia 5 valores Inteiros. A seguir mostre quantos valores digitados foram pares, quantos valores digitados foram ímpares, quantos valores digitados foram positivos e quantos valores digitados foram negativos.
+*/
+
+using System;
+
+namespace CursoNelioAlvesLPA {
+    class Program {
+        static void Main(string[] args) {
+
+            int num, pares, impares, positivos, negativos;
+
+            pares = 0;
+            impares = 0;
+            positivos = 0;
+            negativos = 0;
+
+            Console.WriteLine("Digite cinco valores inteiros quaisquer: ");
+
+            num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0) {
+                pares++;
+            }
+            else {
+                impares++;
+            }
+            if (num > 0) {
+                positivos++;
+            }
+            else if (num < 0) {
+                negativos++;
+            }
+
+            num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0) {
+                pares++;
+            }
+            else {
+                impares++;
+            }
+            if (num > 0) {
+                positivos++;
+            }
+            else if (num < 0) {
+                negativos++;
+            }
+
+            num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0) {
+                pares++;
+            }
+            else {
+                impares++;
+            }
+            if (num > 0) {
+                positivos++;
+            }
+            else if (num < 0) {
+                negativos++;
+            }
+
+            num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0) {
+                pares++;
+            }
+            else {
+                impares++;
+            }
+            if (num > 0) {
+                positivos++;
+            }
+            else if (num < 0) {
+                negativos++;
+            }
+
+            num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0) {
+                pares++;
+            }
+            else {
+                impares++;
+            }
+            if (num > 0) {
+                positivos++;
+            }
+            else if (num < 0) {
+                negativos++;
+            }
+
+            Console.WriteLine(pares + " valor(es) par(es)");
+            Console.WriteLine(impares + " valor(es) impar(es)");
+            Console.WriteLine(positivos + " valor(es) positivo(s)");
+            Console.WriteLine(negativos + " valor(es) negativo(s)");
+        }
+    }
+}
